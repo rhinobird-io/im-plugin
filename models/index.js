dd47ed6f76615f3235ed3cd8f7938e361ccbb964
chadliu23 <liu_q@mail.worksap.com>
2015-02-04 10:02:43 +0800

    team-work/team-work#65 follow heroku specification

--- models/index.js
+++ models/index.js
@@ -6,8 +6,25 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = null;
 var db        = {};
+
+  if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+
+    sequelize = new Sequelize(match[5], match[1], match[2], {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    });
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+
 
 fs
   .readdirSync(__dirname)
